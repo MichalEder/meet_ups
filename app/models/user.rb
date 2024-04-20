c4100136c3af632a38280d9b45535c5f6760b2f0
MichalEder <103189873+MichalEder@users.noreply.github.com>
2024-04-20 17:46:33 +0200

    session control

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :meetup_registrations
+    has_many :registered_meetups, through: :meetup_registrations, source: :meetup
 
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, mesasge: "Must be a valid email address!"}
 end
