6cba5ed018264b3d2c28ca718d7fde4c6c85f157
MichalEder <103189873+MichalEder@users.noreply.github.com>
2024-04-20 17:45:35 +0200

    user structure refactor

--- app/models/organizer.rb
+++
@@ -1,5 +0,0 @@
-class Organizer < ApplicationRecord
-    has_secure_password
-
-    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, mesasge: "Must be a valid email address!"}
-end
