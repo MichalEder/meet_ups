26b26a23588512dde577eccc3a8cfaea2ca66b37
MichalEder <103189873+MichalEder@users.noreply.github.com>
2024-04-20 17:44:31 +0200

    index view, controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,5 @@
 class MainController < ApplicationController
     def index
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        #flash.now[:notice] = "Logged successfully"
-        #flash.now[:alert] = "Invalid credentials"
-        end
+        @meetups = Meetup.all
     end
 end
